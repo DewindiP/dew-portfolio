78f22e719e12879341d00baf156da5c00d8cfd2f
Pramodi Dewindi <pramodidewindi@gmail.com>
2025-06-06 22:22:34 +0530

    ,,

--- src/components/contactMeSection/ContactForm.jsx
+++ src/components/contactMeSection/ContactForm.jsx
@@ -6,7 +6,7 @@ import { fadeIn } from "../../framerMotion/variants";
 const ContactForm = () => {
   return (
     <div className="w-full px-2 sm:px-4 md:px-0 py-6 flex justify-center">
-        <form className="flex flex-col gap-4 p-2.5 ">
+        <form className="flex flex-col gap-4 w-full max-w-2xl p-2.5 ">
             <input 
              type="text" 
              placeholder="Your Name" 
