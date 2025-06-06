ecc80c6bdae8a642d699453217cd95aa095f6818
Pramodi Dewindi <pramodidewindi@gmail.com>
2025-06-06 18:13:31 +0530

    cc

--- src/components/contactMeSection/ContactForm.jsx
+++ src/components/contactMeSection/ContactForm.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
+import { motion } from "framer-motion";
+import { fadeIn } from "../../framerMotion/variants";
 
 
 const ContactForm = () => {
   return (
-    <div>
-        <form className="flex flex-col gap-4 p-2.5">
+    <div className="w-full max-w-lg mx-auto py-6">
+        <form className="flex flex-col gap-4 p-2.5 ">
             <input 
              type="text" 
              placeholder="Your Name" 
-             className="h-12 rounded-lg p-2 bg-oliveGreen" required />
+             className="h-12 rounded-lg p-2 bg-oliveGreen text-sageTintedOlive" required />
             <input 
              type="email" 
              placeholder="Your Email" 
-             className="h-12 rounded-lg p-2 bg-oliveGreen" required />
+             className="h-12 rounded-lg p-2 bg-oliveGreen text-sageTintedOlive" required />
             <textarea 
               type="text" 
               placeholder="Message" 
               rows="9" 
               cols="50" 
-              className="rounded-lg p-2 bg-oliveGreen" required/>
+              className="rounded-lg p-2 bg-oliveGreen text-sageTintedOlive" required/>
             <button type="submmit" className="w-full border border-richMetallicGold rounded-lg text-softSage font-semibold text-xl p-2 hover:bg-warmOliveTint transition-all duration-500 cursor-pointer  hover:text-deepForestGreen hover:font-semibold">Send</button>
         </form>
     </div>
