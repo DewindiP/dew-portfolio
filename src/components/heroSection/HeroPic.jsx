c95c22c203e76348b81dcb0e20d3eaf3c914f2dc
Pramodi Dewindi <pramodidewindi@gmail.com>
2025-06-07 20:06:00 +0530

    hero

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -15,7 +15,7 @@ const Heropic = () => {
       <img
         src="/images/myprofile.png"
         alt="Pramodi Dewindi"
-        className="max-h-[325px] w-auto"
+        className="max-h-[350px] w-auto"
       />
       <div className="absolute -z-10 flex justify-center items-center animate-pulse">
         <LiaCircleSolid className="md:h-[90%] sm:h-[120%] min-h-[500px] w-auto text-mutedGold blur-md animate-[spin_20s_linear_infinte]" />
